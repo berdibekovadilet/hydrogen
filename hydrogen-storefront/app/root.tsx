d1d9899e6d6da2c77054501becde6b56ceb33e7f
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-04-25 15:24:59 +0600

    add collections

--- hydrogen-storefront/app/root.tsx
+++ hydrogen-storefront/app/root.tsx
@@ -1,58 +1,62 @@
-import {type LinksFunction, type LoaderArgs} from '@shopify/remix-oxygen';
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useLoaderData,
 } from '@remix-run/react';
-import type {Shop} from '@shopify/hydrogen/storefront-api-types';
+import {Seo} from '@shopify/hydrogen';
+import tailwind from './styles/tailwind-build.css';
 import styles from './styles/app.css';
 import favicon from '../public/favicon.svg';
+import {Layout} from './components/Layout';
 
-export const links: LinksFunction = () => {
-  return [
-    {rel: 'stylesheet', href: styles},
-    {
-      rel: 'preconnect',
-      href: 'https://cdn.shopify.com',
-    },
-    {
-      rel: 'preconnect',
-      href: 'https://shop.app',
-    },
-    {rel: 'icon', type: 'image/svg+xml', href: favicon},
-  ];
+export const links = () => {
+    return [
+        {rel: 'stylesheet', href: tailwind},
+        {rel: 'stylesheet', href: styles},
+        {
+            rel: 'preconnect',
+            href: 'https://cdn.shopify.com',
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://shop.app',
+        },
+        {rel: 'icon', type: 'image/svg+xml', href: favicon},
+    ];
 };
 
-export async function loader({context}: LoaderArgs) {
-  const layout = await context.storefront.query<{shop: Shop}>(LAYOUT_QUERY);
-  return {layout};
+// @ts-ignore
+export async function loader({context}) {
+    const layout = await context.storefront.query(LAYOUT_QUERY);
+    return {layout};
 }
 
 export default function App() {
-  const data = useLoaderData<typeof loader>();
+    const data = useLoaderData();
 
-  const {name} = data.layout.shop;
+    const {name} = data.layout.shop;
 
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <h1>Hello, {name}</h1>
-        <p>This is a custom storefront powered by Hydrogen</p>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width,initial-scale=1"/>
+            <Seo/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+        <Layout title={name}>
+            <Outlet/>
+        </Layout>
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
 
 const LAYOUT_QUERY = `#graphql
